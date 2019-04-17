a0a6726b5ea4f925f110e6a25364e554a2a1a388
Meron Zeru <sologolom@gmail.com>
2019-04-17 13:47:53 -0400

    salary project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        int time;
+        int wage;
+        double netPay;
+        double OVERTIMEWAGE = 1.5;
+
 	// write your code here
+        System.out.println("Enter hours worked");
+        Scanner keyboard = new Scanner(System.in);
+        time = keyboard.nextInt();
+        System.out.println("Enter salary wage");
+        wage = keyboard.nextInt();
+
+        if (time<60 && wage <25){
+            if(time > 35) {
+                int overTime = (time - 35);
+                double overtimePay = (overTime * OVERTIMEWAGE);
+                netPay = ((35 * wage) + (wage * overtimePay));
+            }
+           else {
+                netPay = ( time * wage);
+            }
+            System.out.println("your net pay is " + netPay);
+        }
+        else   System.out.println("that's not a valid entry");
+
     }
 }
